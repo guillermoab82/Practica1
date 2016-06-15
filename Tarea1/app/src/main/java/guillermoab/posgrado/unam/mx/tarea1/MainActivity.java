d872bcb5a03cf93062474747d35aa8d372106bab
Guillermo Alonso <guillermoab@posgrado.unam.mx>
2016-06-15 18:13:05 -0500

    Empezando el proyecto de calcularoda

---
+++ Tarea1/app/src/main/java/guillermoab/posgrado/unam/mx/tarea1/MainActivity.java
@@ -0,0 +1,13 @@
+package guillermoab.posgrado.unam.mx.tarea1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
